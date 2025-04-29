8b2898577c15e5cf95fcc9d07b3332c6e1e431ce
jpattanashetti <jpattanashetti9@gmail.com>
2025-04-29 10:09:05 +0530

    Update Arthope.java

--- Arthope.java
+++ Arthope.java
@@ -9,7 +9,9 @@ public class Arthope {
         System.out.println("Subtraction "+(a-b));
         System.out.println("Multiplication "+(a*b));
         System.out.println("Division "+(a/b));
-
+         System.out.println("Square root of a: " + Math.sqrt(a));
+        System.out.println("Cube root of a: " + Math.cbrt(a));
+        System.out.println("a raised to the power b: " + Math.pow(a, b));
         
     }
 }
